65257683c69ca9e0e52b7178edff66f80d4c30e6
y_megane_IDEA <y.megane88@gmail.com>
2019-06-30 21:38:52 +0900

    132D

--- AtCoder/ABC/ABC132/ABC132D.py
+++ AtCoder/ABC/ABC132/ABC132D.py
@@ -1,35 +1,22 @@
+import sys
+import math
+sys.setrecursionlimit(1000000)
+
 N,blue = map(int,input().split())
 red = N-blue
+MOD = 10**9+7
 
 def cmb(n, r):
-    nCr = {}
-    if r == 0 or r == n: return 1
-    if r == 1: return n
-    if (n,r) in nCr: return nCr[(n,r)]
-    nCr[(n,r)] = cmb(n-1,r) + cmb(n-1,r-1)
-    return nCr[(n,r)]
+    ans = (math.factorial(n) // math.factorial(n-r)) // math.factorial(r)
+    return ans
 
 # 操作回数
-max_opr = min(red+1, blue)
-
-print(max_opr)
-print(blue,red)
-
-for insi in range(max_opr):
-    # 差し込み方
-    slice_cnt = cmb(blue-1,insi)
-
-    red_cnt = 0
-    # 外で使う数
-    for j in range(red-insi):
-        if insi ==0: # 全部外
-            red_cnt += red+1
-            break
-        # 外の並べ方
-        cnt = j+1
-        # 入れる赤の並べ方
-        if insi>0:
-            cnt *= cmb(red-j, insi-1)
-        red_cnt += cnt
-        print(f'i:{insi} j{j} cnt:{cnt} red:{red_cnt}')
-    print(slice_cnt, red_cnt,slice_cnt * red_cnt)
+for i in range(1, blue+1):
+    if red +1 -i >= 0: # 赤の隙間と両端
+        # 青の差し込み箇所（操作回数）
+        ins_ptn = cmb(red+1, i) % MOD
+        # 青の配置方法
+        layout_ptn = cmb(blue-1, i-1) % MOD
+        print(ins_ptn * layout_ptn % MOD)
+    else:
+        print(0)
